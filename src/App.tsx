e98f043efd94eeb89afa3d44d326aa92fc0cb778
Mustafa Elnaquib <mustafa.elnaquib@klarna.com>
2022-04-28 02:01:54 +0200

    -complete vivid lap react test -added Dockerfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [val0, setVal0] = useState("");
+  const [val1, setVal1] = useState("");
+
+  function wc(val: string) {
+    return val.split(' ').filter(Boolean).length;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input
+          onChange={ (event) => {
+            setVal0(event.target.value);
+            event.preventDefault();
+          }}
+        value={val0}
+        />
+        {wc(val0)} Word(s)
+        <input
+          onChange={ (event) => {
+            setVal1(event.target.value);
+            event.preventDefault();
+          }}
+        value={val1}
+        />
+        {wc(val1)} Word(s)
+
+
+        <br/>
+        <br/>
+        {val0 === val1 ? 'Same' : 'Different'}
       </header>
     </div>
   );
